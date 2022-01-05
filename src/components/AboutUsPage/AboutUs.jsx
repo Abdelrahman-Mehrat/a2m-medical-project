089029d8dad0acf6a38c073a5f7ef53d860a5f82
mohamed diab <mohamddiab005@gmail.com>
2022-01-05 20:53:36 +0200

    about page

--- src/components/AboutUsPage/AboutUs.jsx
+++ src/components/AboutUsPage/AboutUs.jsx
@@ -1,6 +1,18 @@
+import './AboutUs.css'
 const AboutUs =()=>{
     return(
-        <div>about us</div>
+        <div class="site-blocks-cover inner-page">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 mx-auto align-self-center">
+            <div class=" text-center">
+              <h1>About Us</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati natus iure voluptatum eveniet harum recusandae ducimus saepe.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
 }
 export default AboutUs;

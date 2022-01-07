637d95ff12dcaade88af7d7f7eb335864ddc49f9
WaleedShalal <waleed.elbana2011@gmail.com>
2022-01-07 17:24:27 +0200

    fix footer style

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -35,25 +35,49 @@ export const Footer = () => {
                 eaque, aut unde.
               </p>
             </div>
-            <div class="col-lg-3 col-md-6  mx-auto mb-5 mb-lg-0">
-              <h3 class="footer-heading mb-4">Quick Links</h3>
-              <ul class="list-unstyled">
-                <li><Link to="SupplementProducts">Supplement</Link></li>
-                <li><Link to="CosmeticsProducts">Cosmetics</Link></li>
-                <li> <Link to="NutretionProducts">Nutrition</Link> </li>               
+          </div>
+ 
+          <div class='col-lg-3 col-md-6  mx-auto mb-5 mb-lg-0'>
+            <h3 class='footer-heading mb-4'>Quick Links</h3>
+            <ul class='list-unstyled'>
+              <li>
+                <Link to='SupplementProducts'>Supplement</Link>
+              </li>
+              <li>
+                <Link to='CosmeticsProducts'>Cosmetics</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to='NutretionProducts'>Nutrition</Link>{" "}
+              </li>
+            </ul>
+          </div>
+
+          <div class='col-md-6 col-lg-3'>
+            <div class='block-5 mb-5'>
+              <h3 class='footer-heading mb-4'>Contact Info</h3>
+              <ul class='list-unstyled'>
+                <li class='address'>
+                  <i class='fas fa-map-marker-alt'></i>
+                  <a href='https://goo.gl/maps/tSGQYZRP5PvbSNNu7'>
+                    22 m Hadayek al-hram Menkaure Gate Giza
+                  </a>
+                </li>
+                <li class='phone'>
+                  <i class='fas fa-phone-alt'></i>
+                  <a href='tel://23923929210'>+20-01100810690</a>
+                </li>
+                <li class='phone'>
+                  <i class='fas fa-phone-alt'></i>
+                  <a href='tel://23923929210'>+20-01119998720</a>
+                </li>
+                <li class='email'>
+                  <i class='fas fa-envelope'></i>
+                  <a href='mailto: a2mmedical20@outlook.com'>
+                    a2mmedical20@outlook.com
+                  </a>
+                </li>
               </ul>
-            </div>
-  
-            <div class="col-md-6 col-lg-3">
-              <div class="block-5 mb-5">
-                <h3 class="footer-heading mb-4">Contact Info</h3>
-                <ul class="list-unstyled">
-                  <li class="address"><i class="fas fa-map-marker-alt"></i><a href="https://goo.gl/maps/tSGQYZRP5PvbSNNu7">22 m Hadayek al-hram Menkaure Gate Giza</a></li>
-                  <li class="phone"><i class="fas fa-phone-alt"></i><a href="tel://23923929210">+20-01100810690</a></li>
-                  <li class="phone"><i class="fas fa-phone-alt"></i><a href="tel://23923929210">+20-01119998720</a></li>
-                  <li class="email"><i class="fas fa-envelope"></i><a href="mailto: a2mmedical20@outlook.com">a2mmedical20@outlook.com</a></li>
-                </ul>
-              </div>
             </div>
           </div>
         </div>
--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -1,89 +1,147 @@
-import React, { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import { FiAlignRight, FiXCircle, FiChevronDown } from "react-icons/fi";
 import logoImg from "./logoPic.ico";
 // import logo from '../../img/logo.png';
 
-import './NavBar.css'
+import "./NavBar.css";
 export const NavBar = () => {
+  const [isMenu, setisMenu] = useState(false);
+  const [isResponsiveclose, setResponsiveclose] = useState(false);
+  const toggleClass = () => {
+    setisMenu(isMenu === false ? true : false);
+    setResponsiveclose(isResponsiveclose === false ? true : false);
+  };
 
-    const [isMenu, setisMenu] = useState(false);
-    const [isResponsiveclose, setResponsiveclose] = useState(false);
-    const toggleClass = () => {
-        setisMenu(isMenu === false ? true : false);
-        setResponsiveclose(isResponsiveclose === false ? true : false);
-    };
+  let boxClass = ["main-menu menu-right menuq1"];
+  if (isMenu) {
+    boxClass.push("menuq2");
+  } else {
+    boxClass.push("");
+  }
 
-    let boxClass = ["main-menu menu-right menuq1"];
-    if (isMenu) {
-        boxClass.push('menuq2');
-    } else {
-        boxClass.push('');
-    }
+  const [isMenuSubMenu, setMenuSubMenu] = useState(false);
 
-    const [isMenuSubMenu, setMenuSubMenu] = useState(false);
+  const toggleSubmenu = () => {
+    setMenuSubMenu(isMenuSubMenu === false ? true : false);
+  };
 
-    const toggleSubmenu = () => {
-        setMenuSubMenu(isMenuSubMenu === false ? true : false);
-    };
+  let boxClassSubMenu = ["sub__menus"];
+  if (isMenuSubMenu) {
+    boxClassSubMenu.push("sub__menus__Active");
+  } else {
+    boxClassSubMenu.push("");
+  }
 
-    let boxClassSubMenu = ["sub__menus"];
-    if (isMenuSubMenu) {
-        boxClassSubMenu.push('sub__menus__Active');
-    } else {
-        boxClassSubMenu.push('');
-    }
+  return (
+    <header className='header__middle'>
+      <div className='container'>
+        <div className='row d-flex align-items-center'>
+          {/* Add Logo  */}
+          <div className='header__middle__logo'>
+            <NavLink exact activeClassName='is-active' to='/'>
+              <img className='logoImg' src={`${logoImg}`} alt='' />
+            </NavLink>
+          </div>
 
+          <div className='header__middle__menus'>
+            <nav className='main-nav '>
+              {/* Responsive Menu Button */}
+              {isResponsiveclose === true ? (
+                <>
+                  <span
+                    className='menubar__button'
+                    style={{ display: "none" }}
+                    onClick={toggleClass}>
+                    {" "}
+                    <FiXCircle />{" "}
+                  </span>
+                </>
+              ) : (
+                <>
+                  <span
+                    className='menubar__button'
+                    style={{ display: "none" }}
+                    onClick={toggleClass}>
+                    {" "}
+                    <FiAlignRight />{" "}
+                  </span>
+                </>
+              )}
 
-
-    return (
-        <header className="header__middle">
-            <div className="container">
-                <div className="row d-flex align-items-center">
-
-                    {/* Add Logo  */}
-                    <div className="header__middle__logo">
-                        <NavLink exact activeClassName='is-active' to="/">
-                        <img className='logoImg'  src={`${logoImg}`} alt="" />
-                        </NavLink>
-                    </div>
-
-                    <div className="header__middle__menus">
-                        <nav className="main-nav " >
-
-                            {/* Responsive Menu Button */}
-                            {isResponsiveclose === true ? <>
-                                <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
-                            </> : <>
-                                <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
-                            </>}
-                          
-                            <ul className={boxClass.join(' ')}>
-                                <li className="menu-item" >
-                                    <NavLink exact activeClassName='is-active' onClick={toggleClass} to={`/`}> Home </NavLink>
-                                </li>
-                                <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/About`}> About </NavLink> </li>
-                                <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="#"> Category <FiChevronDown /> </Link>
-                                    <ul className={boxClassSubMenu.join(' ')} >
-                                        <li> <NavLink onClick={toggleClass} activeClassName='is-active' to={`/SupplementProducts`}> Supplements </NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/CosmeticsProducts`}> Cosmetics </NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/NutretionProducts`}> Nutrition </NavLink> </li>
-
-                                    </ul>
-                                </li>
-                                <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={``}> Contact </NavLink> </li>
-
-                            </ul>
-                        </nav>
-                    </div>
-
-
-
-
-
-                </div>
-            </div>
-        </header>
-    )
-}
-
+              <ul className={boxClass.join(" ")}>
+                <li className='menu-item'>
+                  <NavLink
+                    exact
+                    activeClassName='is-active'
+                    onClick={toggleClass}
+                    to={`/`}>
+                    {" "}
+                    Home{" "}
+                  </NavLink>
+                </li>
+                <li className='menu-item '>
+                  <NavLink
+                    onClick={toggleClass}
+                    activeClassName='is-active'
+                    to={`/About`}>
+                    {" "}
+                    About{" "}
+                  </NavLink>{" "}
+                </li>
+                <li
+                  onClick={toggleSubmenu}
+                  className='menu-item sub__menus__arrows'>
+                  {" "}
+                  <Link to='#'>
+                    {" "}
+                    Category <FiChevronDown />{" "}
+                  </Link>
+                  <ul className={boxClassSubMenu.join(" ")}>
+                    <li>
+                      {" "}
+                      <NavLink
+                        onClick={toggleClass}
+                        activeClassName='is-active'
+                        to={`/SupplementProducts`}>
+                        {" "}
+                        Supplements{" "}
+                      </NavLink>{" "}
+                    </li>
+                    <li>
+                      <NavLink
+                        onClick={toggleClass}
+                        activeClassName='is-active'
+                        to={`/CosmeticsProducts`}>
+                        {" "}
+                        Cosmetics{" "}
+                      </NavLink>{" "}
+                    </li>
+                    <li>
+                      <NavLink
+                        onClick={toggleClass}
+                        activeClassName='is-active'
+                        to={`/NutretionProducts`}>
+                        {" "}
+                        Nutrition{" "}
+                      </NavLink>{" "}
+                    </li>
+                  </ul>
+                </li>
+                <li className='menu-item '>
+                  <NavLink
+                    onClick={toggleClass}
+                    activeClassName='is-active'
+                    to={"/contactus"}>
+                    {" "}
+                    Contact Us
+                  </NavLink>{" "}
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};

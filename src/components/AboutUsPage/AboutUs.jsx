bb9661af8287ed4e4664f9d02219cbb2d9d06c60
mohamed diab <mohamddiab005@gmail.com>
2022-01-08 18:37:07 +0200

    about us page

--- src/components/AboutUsPage/AboutUs.jsx
+++ src/components/AboutUsPage/AboutUs.jsx
@@ -1,18 +1,300 @@
 import './AboutUs.css'
-const AboutUs =()=>{
-    return(
-        <div class="site-blocks-cover inner-page">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-7 mx-auto align-self-center">
-            <div class=" text-center">
-              <h1>About Us</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati natus iure voluptatum eveniet harum recusandae ducimus saepe.</p>
+const AboutUs = () => {
+  return (
+    <>
+      <div class="site-blocks-cover inner-page">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7 mx-auto align-self-center">
+              <div class=" text-center">
+                <h1>About Us</h1>
+                <p>A2M Medical is a leading
+                  pharmaceutical company in the
+                  Egyptian market, established in 2020.
+                  A2M Medical provides
+                  pharmaceuticals Drugs, babies
+                  nutrition formulas, nutraceuticals and
+                  cosmeceuticals that improve the life of
+                  the family and society, providing them
+                  with the best in terms of quality and
+                  cost in Egypt and the globe. </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+
+      <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-6 slogan">
+              <div class="block-16">
+                <h2 class="text-black">OUR SLOGAN</h2>
+              </div>
+            </div>
+            <div class="col-md-1">
+
+            </div>
+            <div class="col-md-5">
+              <div class="site-section-heading pt-3 mb-4">
+              </div>
+              <p>
+                Care You Deserve
+                This is the slogan of A2M medical
+                which reflects the real core of our goals and our sense
+                of social responsibility as it is
+                founded in our belief that every
+                citizen has the right to live a good
+                life and enjoy adequate
+                health care and gets access to
+                pharmaceutical products that are
+                characterized with high quality. Therefore, all our
+                products and services aim to
+                enhancing such belief starting from
+                the selection of our products
+                through stages of production and
+                marketing.
+                This slogan also applies to all our
+                employees, as we strive to create an
+                ideal working atmosphere for our
+                employees to motivate them to
+                work actively and effectively to serve
+                our clients.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section bg-white custom-border-bottom" data-aos="fade">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-12 mr-auto ">
+              <div class="site-section-heading pt-3 mb-4">
+                <h2 class="text-black">We Are Trusted Company</h2>
+              </div>
+              <p class="text-black fs-5">
+                "Our vision is to be a leading
+                pharmaceutical company in Egypt and to
+                become a significant global player by
+                providing high quality, affordable and
+                innovative solutions in medicine and
+                treatment."
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-6 slogan">
+              <div class="block-16">
+                <h2 class="text-black">Mission</h2>
+              </div>
+            </div>
+            <div class="col-md-1">
+
+            </div>
+            <div class="col-md-5">
+              <div class="site-section-heading pt-3 mb-4">
+              </div>
+              <p>
+                We shall provide total customer satisfaction and achieve
+                leadership in chosen markets, products and services across
+                the globe, through excellence in technology, based on worldclass research and development.
+                We will discover, develop and successfully market
+                pharmaceutical products to prevent, alleviate and cure disease
+                We are responsible to the society. We shall be good corporate
+                citizens and will be driven by high ethical standards in our
+                practices.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="site-section bg-white custom-border-bottom" data-aos="fade">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-12 mr-auto ">
+              <div class="site-section-heading pt-3 mb-4">
+                <h2 class="text-black">The 10 CORE VALUES of A2M Medica</h2>
+              </div>
+              <p class="text-black">
+                Our core values are the driving force behind who we are and what we
+                do. A2M medical stands on a legacy of service excellence and
+                traditions that are supported by our commitment to integrity and trust,
+                teamwork, a passion for excellence, innovation,fairness & honesty,motivation &
+                commitment,achievement,quality,family & loyalty and personal growth. It is these
+                core values that inspire us to always put first the needs and well-being
+                of the people and communities we serve.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+        <div class="container">
+          <div class="row justify-content-center mb-5">
+            <div class="col-md-7 site-section-heading text-center pt-4">
+              <h2>The 10 CORE VALUES of A2M Medical</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 col-lg-12 mb-5">
+              <div class="block-38 ">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    <h3 class="block-38-heading h4">1-Integrity & Trust</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Build open honest relationships
+                      Built upon unwavering integrity, trust is the essence. We must uphold
+                      our integrity and the trust placed in us in all actions we take.Build open honest relationships
+                      Built upon unwavering integrity, trust is the essence. We must uphold
+                      our integrity and the trust placed in us in all actions we take.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    <h3 class="block-38-heading h4">2-Teamwork </h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Team means together everyone achieve more.
+                      At A2M Medical, We believe teamwork is the key to delivering
+                      innovative solutions that help everybody win. </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_3.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">3-Innovation</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Be creative, open-minded and thinking out of the box.
+                      We believe true innovation is the combination of creativity, openmindedness and deep understanding of the marketplace.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">4-Passion for Excellence</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>When the best is not enough
+                      We are passionate about delivering the best results every time and
+                      are determined to do even better the next time.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38 ">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">5-Achievement
+                    </h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>“greatest achievements are often directly related to greatest challenges”…..
+                      Kirsty Allen </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">6-Quality
+                    </h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Our concern is Quality not Quantity
+                      We believe that " When quality is in our minds. The defect will stay behind</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">7-Fairness & honesty
+                    </h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Treating others with fairness and honesty is like the«rain»that helps them
+                      to grow and be fruitful.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">8-Motivation & Commitment</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>We believe that “ Motivation is what gets you started. Commitment is what
+                      keeps you going.”</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38 ">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">9-Personal Growth</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>Keeping growth and learning
+                      We are driven in the pursuit of personal growth and learning.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <div class="block-38">
+                <div class="block-38-img">
+                  <div class="block-38-header">
+                    {/* <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4"> */}
+                    <h3 class="block-38-heading h4">10-Family & Loyalty</h3>
+                  </div>
+                  <div class="block-38-body">
+                    <p>A2M Medical is more than a Company,It's a Family.
+                      Loyalty “Always staying true to our ideals and people”</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
+  )
 }
 export default AboutUs;
--- src/components/HomeAboutUs/AboutUsMain.jsx
+++ src/components/HomeAboutUs/AboutUsMain.jsx
@@ -3,25 +3,30 @@ import logoPic from "./logoPic.jpg"
 const AboutUsMain = () => {
   return (
     <section className="Home__AboutUs head__animation py-4">
-    <div className="container ">
-      <div className="row">
-        <div className="col-md-8">
-          <div className="p-2">
-            <h2 className="section__header">About us</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-              molestias eligendi minima velit. Facere, pariatur debitis
-              consectetur esse deserunt accusantium adipisci tempora recusandae
-              numquam, praesentium non, perferendis doloremque voluptates
-              quidem.
-            </p>
+      <div className="container ">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="p-2">
+              <h2 className="section__header">About us</h2>
+              <p className="about-us">
+                A2M Medical is a leading
+                pharmaceutical company in the
+                Egyptian market, established in 2020.
+                A2M Medical provides
+                pharmaceuticals Drugs, babies
+                nutrition formulas, nutraceuticals and
+                cosmeceuticals that improve the life of
+                the family and society, providing them
+                with the best in terms of quality and
+                cost in Egypt and the globe.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 p-4 text-center">
+            <img src={logoPic} className="w-100" alt="" />
           </div>
         </div>
-        <div className="col-md-4 p-4 text-center">
-              <img src={logoPic} className="w-100" alt="" /> 
-        </div>
       </div>
-    </div>
     </section>
   );
 };

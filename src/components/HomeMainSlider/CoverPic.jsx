ad82d0fd5d1f1fd5b5961fd8f1ca258c16c56b4d
WaleedShalal <waleed.elbana2011@gmail.com>
2022-01-11 01:14:10 +0200

    edit style

--- src/components/HomeMainSlider/CoverPic.jsx
+++ src/components/HomeMainSlider/CoverPic.jsx
@@ -3,7 +3,6 @@ import imgTwo from "../../assets/image/home/1 english.jpg";
 import imgThree from "../../assets/image/home/3.jpg";
 import imgFour from "../../assets/image/home/2 english.jpg";
 import imgFive from "../../assets/image/home/nutriCover.jpg";
-
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import "./CoverPic.css";
@@ -24,7 +23,7 @@ export const CoverPic = () => {
           <div>
             <img src={imgFive} alt='' />
           </div>
-          
+
           <div>
             <img src={imgFour} alt='' />
           </div>
@@ -45,19 +44,19 @@ export const CoverPic = () => {
         <FontAwesomeIcon icon={faAddressCard} className="iconn" />
             <p>Supplements</p>
         </div> */}
-            <div className='fadeInLeftBig col-md-4 col-sm-6 py-1  px-4 px-sm-3 px-md-2'>
+            <div className='col-md-4 col-sm-6 py-1  px-4 px-sm-3 px-md-2'>
               <div className='p-4  contactUs__data text-center'>
                 <i className='fas fa-tachometer-alt fa-2x mb-2'></i>
                 <p>Supplements</p>
               </div>
             </div>
-            <div className='fadeInLeftBig col-md-4  col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
+            <div className='col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
               <div className='p-4 contactUs__data text-center'>
                 <i className='fas fa-adjust fa-2x mb-2'></i>
                 <p>Cosmetics</p>
               </div>
             </div>
-            <div className='fadeInLeftBig col-md-4  col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
+            <div className='col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
               <div className='p-4 contactUs__data text-center'>
                 <i className='fas fa-baby-carriage fa-2x mb-2'></i>
                 <p>Nutrition</p>

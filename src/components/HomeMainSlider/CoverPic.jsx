c82a023583a7a3a0ac89ece2bf32a02da51e5a9a
abdo <abdo@info.com>
2022-06-14 14:10:52 -0700

    update map-number in footer

--- src/components/HomeMainSlider/CoverPic.jsx
+++ src/components/HomeMainSlider/CoverPic.jsx
@@ -5,53 +5,53 @@ import imgTwo from "../../assets/image/home/1 english.jpg";
 import imgThree from "../../assets/image/home/3.jpg";
 import imgFour from "../../assets/image/home/2 english.jpg";
 import imgFive from "../../assets/image/home/nutriCover.jpeg";
-import "./CoverPic.css";
+import "./coverPic.css";
 const CoverPic = () => {
   return (
     <>
-      <div className='mainImage'>
+      <div className="mainImage">
         <Carousel autoPlay={true} infiniteLoop={true}>
           <div>
-            <img src={imgTwo} alt='' />
+            <img src={imgTwo} alt="" />
           </div>
           <div>
-            <img src={imgOne} alt='' />
+            <img src={imgOne} alt="" />
           </div>
           <div>
-            <img src={imgThree} alt='' />
+            <img src={imgThree} alt="" />
           </div>
           <div>
-            <img src={imgFive} alt='' />
+            <img src={imgFive} alt="" />
           </div>
           <div>
-            <img src={imgFour} alt='' />
+            <img src={imgFour} alt="" />
           </div>
         </Carousel>
       </div>
-      <div className='contactUs p-5'>
-        <div className='container'>
-          <div className='row justify-content-center'>
-            <div className='col-lg-3 col-md-4 col-sm-6 py-1  px-4 px-sm-3 px-md-2'>
-              <div className='p-4  contactUs__data text-center'>
-                <i className='fas fa-tachometer-alt fa-2x mb-2'></i>
+      <div className="contactUs p-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-3 col-md-4 col-sm-6 py-1  px-4 px-sm-3 px-md-2">
+              <div className="p-4  contactUs__data text-center">
+                <i className="fas fa-tachometer-alt fa-2x mb-2"></i>
                 <p>Supplements</p>
               </div>
             </div>
-            <div className='col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
-              <div className='p-4 contactUs__data text-center'>
-                <i className='fas fa-adjust fa-2x mb-2'></i>
+            <div className="col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2">
+              <div className="p-4 contactUs__data text-center">
+                <i className="fas fa-adjust fa-2x mb-2"></i>
                 <p>Cosmetics</p>
               </div>
             </div>
-            <div className='col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
-              <div className='p-4 contactUs__data text-center'>
-                <i className='fas fa-baby-carriage fa-2x mb-2'></i>
+            <div className="col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2">
+              <div className="p-4 contactUs__data text-center">
+                <i className="fas fa-baby-carriage fa-2x mb-2"></i>
                 <p>Nutrition</p>
               </div>
             </div>
-            <div className='col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2'>
-              <div className='p-4 contactUs__data text-center'>
-                <i className='fas fa-mortar-pestle fa-2x mb-2'></i>
+            <div className="col-lg-3 col-md-4 col-sm-6 py-1 px-4 px-sm-3 px-md-2">
+              <div className="p-4 contactUs__data text-center">
+                <i className="fas fa-mortar-pestle fa-2x mb-2"></i>
                 <p>Pharmaceuticals</p>
               </div>
             </div>
--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -3,67 +3,69 @@ import MapImage from "../../assets/image/home/location.png";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <footer className='site-footer'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-2 col-md-6 mb-5 mb-lg-0'>
-            <h3 className='footer-heading mb-4'>Quick Links</h3>
-            <ul className='quick__liksList list-unstyled'>
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2 col-md-6 mb-5 mb-lg-0">
+            <h3 className="footer-heading mb-4">Quick Links</h3>
+            <ul className="quick__liksList list-unstyled">
               <li>
-                <i className='fas fa-chevron-circle-right pe-2'></i>
-                <Link to='SupplementProducts'>Supplement</Link>
+                <i className="fas fa-chevron-circle-right pe-2"></i>
+                <Link to="SupplementProducts">Supplement</Link>
               </li>
               <li>
-                <i className='fas fa-chevron-circle-right pe-2'></i>
-                <Link to='CosmeticsProducts'>Cosmetics</Link>
+                <i className="fas fa-chevron-circle-right pe-2"></i>
+                <Link to="CosmeticsProducts">Cosmetics</Link>
               </li>
               <li>
-                <i className='fas fa-chevron-circle-right pe-2'></i>
-                <Link to='nutretionproducts'>Nutrition</Link>
+                <i className="fas fa-chevron-circle-right pe-2"></i>
+                <Link to="nutretionproducts">Nutrition</Link>
               </li>
               <li>
-                <i className='fas fa-chevron-circle-right pe-2'></i>
-                <Link to='PharmaceuticalsProducts'>Pharmaceuticals</Link>
+                <i className="fas fa-chevron-circle-right pe-2"></i>
+                <Link to="PharmaceuticalsProducts">Pharmaceuticals</Link>
               </li>
             </ul>
           </div>
-          <div className='col-md-6 col-lg-4'>
-            <div className='block-5 mb-5'>
-              <h3 className='footer-heading mb-4'>Contact Info</h3>
-              <ul className='list-unstyled'>
-                <li className='address'>
-                  <i className='fas fa-map-marker-alt'></i>
-                  <Link to='https://goo.gl/maps/tSGQYZRP5PvbSNNu7'>
+          <div className="col-md-6 col-lg-4">
+            <div className="block-5 mb-5">
+              <h3 className="footer-heading mb-4">Contact Info</h3>
+              <ul className="list-unstyled">
+                <li className="address">
+                  <i className="fas fa-map-marker-alt"></i>
+                  <a
+                    target="_blank"
+                    href="https://www.google.com/maps/place/A2Mmedical/@29.9538623,31.1038839,19.83z/data=!4m5!3m4!1s0x14584f92e819c4bd:0x7d979ae4cdfe8de7!8m2!3d29.9539511!4d31.103792?hl=en-EG"
+                  >
                     22 M Hadayek Al-hram Menkaure Gate Giza
-                  </Link>
+                  </a>
                 </li>
-                <li className='phone'>
-                  <i className='fas fa-phone-alt'></i>
-                  {/* <Link to='tel://23923929210'>+20-01100810690</Link> */}
-                  <a href='tel:+20-01100810690'>+20-01100810690</a>
+                <li className="phone">
+                  <i className="fas fa-phone-alt"></i>
+                  <a href="tel:+20-01100810690">+20-01100810690</a>
                 </li>
-                {/* <li className='phone'>
-                  <i className='fas fa-phone-alt'></i>
-                  <Link to='tel://23923929210'>+20-01119998720</Link>
-                  <a href='tel:+20-01119998720'>+20-01119998720</a>
-                </li> */}
-                <li className='email'>
-                  <i className='fas fa-envelope'></i>
-                  {/* <Link to='mailto: a2mmedical20@outlook.com'>
-                    a2mmedical20@outlook.com
-                  </Link> */}
-                  <a href='mailto:a2mmedical20@outlook.com'>
-                    a2mmedical20@outlook.com
+
+                <li className="email">
+                  <i className="fas fa-envelope"></i>
+
+                  <a href="mailto:a2mmedical20@gmail.com">
+                    a2mmedical20@gmail.com
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div className='col-md-12 col-lg-6 mb-4 mb-lg-0'>
-            <h3 className='footer-heading mb-4'>Map Location</h3>
-            <figure className='mb-0'>
-              <img className='w-100' src={MapImage} alt='' />
-            </figure>
+          <div className="col-md-12 col-lg-6 mb-4 mb-lg-0">
+            <h3 className="footer-heading mb-4">Map Location</h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d485.02838881787136!2d31.103883916478615!3d29.95386230921724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14584f92e819c4bd%3A0x7d979ae4cdfe8de7!2sA2Mmedical!5e0!3m2!1sen!2seg!4v1655240081677!5m2!1sen!2seg"
+              width="600"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
       </div>
